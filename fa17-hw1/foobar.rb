5991a96a829f27ccc2bce73d209457d5d4e54f0a
Henry Ao <hao1@berkeley.edu>
2017-09-20 16:58:15 -0700

    hw1 complete

--- fa17-hw1/foobar.rb
+++ fa17-hw1/foobar.rb
@@ -1,8 +1,14 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    a.map!(&:to_i)
+    a2 = a.map{|i| i + 2}
+    toReturn = 0
+    a2.delete_if{|num| num % 2 == 1}
+    a2.uniq!
+    a2.delete_if{|num| num > 10}
+    a2.map{|i| toReturn = toReturn + i}
+    return toReturn
   end
 end
 
